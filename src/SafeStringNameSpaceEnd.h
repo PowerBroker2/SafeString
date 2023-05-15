888b36fabdf532988bd6e7697cbaaf799b1af2b5
drmpf <drmpf@users.noreply.github.com>
2023-05-15 11:44:29 +1000

    v4_1_27
    V4.1.27 revised defines for Arduino Zero  plus PlatformIO versions

--- src/SafeStringNameSpaceEnd.h
+++ src/SafeStringNameSpaceEnd.h
@@ -1,7 +1,7 @@
 #ifndef SAFE_STRING_NAMESPACE_END_H
 #define SAFE_STRING_NAMESPACE_END_H
 
-#if defined(ARDUINO_SAMD_ZERO) || defined(MEGATINYCORE_MAJOR) || defined(MOTEINO_M0) || defined(RFGATEWAY_M4) || defined(CURRENT_RANGER) || defined(__SAMD51__)
+#if  defined(MEGATINYCORE_MAJOR) || defined(MOTEINO_M0) || defined(RFGATEWAY_M4) || defined(CURRENT_RANGER) || defined(__SAMD51__)
 // to skip this for SparkFun RedboardTurbo and MegaTinyCore and MOTEINO boards and Adafruit M4 boards
 #else
 #if defined(ARDUINO_ARDUINO_NANO33BLE) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_MEGAAVR)  || defined(ARDUINO_ARCH_MBED_RP2040) || defined(ARDUINO_ARCH_RP2040)
