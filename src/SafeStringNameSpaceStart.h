e0f42ab769a5cae8d2fa9c2a1d2057ab60e77479
drmpf <drmpf@users.noreply.github.com>
2024-02-09 17:35:41 +1100

    V4_1_31
    V4.1.31 added support for other Arduino MBED boards like Opta

--- src/SafeStringNameSpaceStart.h
+++ src/SafeStringNameSpaceStart.h
@@ -4,7 +4,7 @@
 #if defined(MEGATINYCORE_MAJOR) || defined(MOTEINO_M0) || defined(RFGATEWAY_M4) || defined(CURRENT_RANGER) || defined(__SAMD51__)
 // to skip this for SparkFun RedboardTurbo and MegaTinyCore and MOTEINO boards and Adafruit M4 boards
 #else
-#if defined(ARDUINO_ARDUINO_NANO33BLE) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_MEGAAVR) || defined(ARDUINO_ARCH_MBED_RP2040) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_UNOR4_MINIMA)
+#if defined(ARDUINO_ARDUINO_NANO33BLE) || defined(ARDUINO_ARCH_SAMD) || defined(ARDUINO_ARCH_MEGAAVR) || defined(ARDUINO_ARCH_MBED_RP2040) || defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_UNOR4_WIFI) || defined(ARDUINO_UNOR4_MINIMA) || defined(ARDUINO_ARCH_MBED)
 namespace arduino {
 #endif
 #endif // #if defined(MEGATINYCORE_MAJOR) ...
